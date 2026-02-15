df53a5bbc7fb80d09dbf75f02b2e11a730d06496
Ashwin-arul2112 <ashwinarul2112@gmail.com>
2026-02-15 10:36:43 +0530

    S14

--- app/ar/ARScene.tsx
+++ app/ar/ARScene.tsx
@@ -22,7 +22,7 @@ const startAR = async () => {
 function WorldLockedModel(){
 
   const ref = useRef<any>(null)
-  const { scene } = useGLTF("/models/model.glb")
+  const { scene } = useGLTF("/models/wand.glb")
   const { session } = useXR()
 
   const [placed,setPlaced] = useState(false)
